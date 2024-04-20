f19e67e8c72c3aedf79213a6c0ecb741fe62c855
vanitha-himagirish <55011879+vanitha-himagirish@users.noreply.github.com>
2024-04-19 22:38:06 -0500

    Create 584_Find Customer Referee.sql

---
+++ leetcode/584_Find Customer Referee.sql
@@ -0,0 +1,17 @@
+/*Question: 584 Find Customer Referee
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| referee_id  | int     |
++-------------+---------+
+In SQL, id is the primary key column for this table.
+Each row of this table indicates the id of a customer, their name, and the id of the customer who referred them.
+ 
+
+Find the names of the customer that are not referred by the customer with id = 2.
+
+Answer:
+*/
+SELECT name FROM Customer where isnull(referee_id,1)<>2
