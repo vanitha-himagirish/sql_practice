7a62c440947fac358b5b5cf16be4ac2a75fc7dea
vanitha-himagirish <55011879+vanitha-himagirish@users.noreply.github.com>
2024-04-19 22:20:58 -0500

    Update SQL_leetcode_1757_Recyclable and Low Fat Products.sql

--- SQL_leetcode_1757_Recyclable and Low Fat Products.sql
+++ SQL_leetcode_1757_Recyclable and Low Fat Products.sql
@@ -1,4 +1,4 @@
-*Question: 1757. Recyclable and Low Fat Products
+/*Question: 1757. Recyclable and Low Fat Products
 +-------------+---------+
 | Column Name | Type    |
 +-------------+---------+
@@ -13,7 +13,7 @@ recyclable is an ENUM (category) of types ('Y', 'N') where 'Y' means this produc
 
 Write a solution to find the ids of products that are both low fat and recyclable.
 
-Return the result table in any order.
+Return the result table in any order.*/
 
-Answer:
+/*Answer:*/
 select product_id from products where low_fats='Y' and recyclable  ='Y'
