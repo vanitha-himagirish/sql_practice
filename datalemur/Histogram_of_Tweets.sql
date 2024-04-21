54cd4db8090324927bb7bb55879636af1fffe4cb
vanitha-himagirish <55011879+vanitha-himagirish@users.noreply.github.com>
2024-04-20 20:47:46 -0500

    Update Histogram_of_Tweets.sql

--- datalemur/Histogram_of_Tweets.sql
+++ datalemur/Histogram_of_Tweets.sql
@@ -4,22 +4,26 @@ Assume you're given a table Twitter tweet data, write a query to obtain a histog
 In other words, group the users by the number of tweets they posted in 2022 and count the number of users in each group.
 
 tweets Table:
+
 Column Name	Type
 tweet_id	integer
 user_id	integer
-msg	string
+msg	       string
 tweet_date	timestamp
+
 tweets Example Input:
-tweet_id	user_id	msg	tweet_date
-214252	111	Am considering taking Tesla private at $420. Funding secured.	12/30/2021 00:00:00
-739252	111	Despite the constant negative press covfefe	01/01/2022 00:00:00
-846402	111	Following @NickSinghTech on Twitter changed my life!	02/14/2022 00:00:00
-241425	254	If the salary is so competitive why won’t you tell me what it is?	03/01/2022 00:00:00
-231574	148	I no longer have a manager. I can't be managed	03/23/2022 00:00:00
+tweet_id	user_id	msg	                                                               tweet_date
+214252	       111	       Am considering taking Tesla private at $420. Funding secured.	       12/30/2021 00:00:00
+739252	       111	       Despite the constant negative press covfefe	                     01/01/2022 00:00:00
+846402	       111	       Following @NickSinghTech on Twitter changed my life!	              02/14/2022 00:00:00
+241425	       254	       If the salary is so competitive why won’t you tell me what it is?	03/01/2022 00:00:00
+231574	       148	       I no longer have a manager. I can't be managed	                     03/23/2022 00:00:00
+
 Example Output:
 tweet_bucket	users_num
-1	2
-2	1
+1	       2
+2	       1
+
 Explanation:
 Based on the example output, there are two users who posted only one tweet in 2022, and one user who posted two tweets in 2022. 
 The query groups the users by the number of tweets they posted and displays the number of users in each group.
